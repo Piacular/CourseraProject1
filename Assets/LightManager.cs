f0fce067e75a1723235fd430ffd435c55fab2441
Piacular <wscottwarner@gmail.com>
2023-01-03 06:12:11 -0500

    Flashlight implementation/URP update
    Changed:
    
    - Converted all textures to URP, took out 5 tall oak trees and replaced them with a new tree that has URP compatible materials.
    - Fixed campfire light shadow animation, now casts more consistent and natural shadows.
    - Changed X of right hand model to more accurately align with controller
    
    Added:
    
    - New tall tree assets to replace Oaks
    - Textured flashlight object (Yay, first blender creation)
    - Sphere colliders for XR interaction in controllers
    - XR Grab interactable on Flashlight
    - Spotlight lightsource on end of flashlight
    - Wrote script to allow flashlight to be toggled on and off when held in the left or right hand
    - Added on/off sounds for flashlight toggling (might remove later, they sound kinda funky, and the controller makes an immersive click anyway)
    - New giant log assets to block off explorable area

---
+++ Assets/LightManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField] GameObject Flashlight;
+    public bool isActive = false;
+    private bool toggling = false;
+    //public AudioSource source;
+    //public AudioClip onSound;
+    //public AudioClip offSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+        Flashlight.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle()
+    {
+        Debug.Log("Toggle() triggered.");
+
+            if (isActive == false)
+            {
+                //source.PlayOneShot(onSound);
+                Debug.Log("Toggle on triggered, isActive = " + isActive);
+                Flashlight.gameObject.SetActive(true);
+                Debug.Log("Setting isActive to true");
+                isActive = true;
+            }
+
+            else
+            {
+                //source.PlayOneShot(offSound);
+                Debug.Log("Toggle off triggered, isActive = " + isActive);
+                Flashlight.gameObject.SetActive(false);
+                Debug.Log("Setting isActive to false");
+                isActive = false;
+            }
+
+            Debug.Log("Toggle() finishing, setting toggling to false");
+
+    }
+}
